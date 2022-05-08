49b8ec774a59832ab0e337afc92c0ea83d49db29
Hamsa DJ <90080171+Hamsa00dj@users.noreply.github.com>
2022-05-08 19:00:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './pages/Home';
+import './App.css';
+import { Pages } from './pages/Pages';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Pages />
+    </div>
+  )
+}
+
+export default App
